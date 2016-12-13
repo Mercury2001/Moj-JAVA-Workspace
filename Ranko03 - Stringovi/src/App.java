6952f9c021bc224137ba3be656afba8ba8ae4be6
Ranko <Ranko>
2016-12-13 11:18:54 +0100

    first commit

---
+++ Ranko03 - Stringovi/src/App.java
@@ -0,0 +1,32 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		//deklarisanje stringa
+		/*String[] words = new String[3];
+		
+		//deklarisanje vrijednosti za string
+		words[0] = "pozdrav, ";
+		words[1] = "kako si ";
+		words[2] = "ti ?";
+		//deklarisanje i definisanje stringa istovremeno
+		String[] fruits = {"jabuka", "banana", "jagoda", "kivi", "jagoda" };
+		// kratka forma FOR petlje
+		//ovaj dio ce se raditi na sljedecim primjerima, za sada ga usvajamo ovakvog
+		for(String fruit: fruits) {
+			//
+			System.out.println(fruit);
+			
+		/* konkatenacija stringova
+		 * */
+	       /* String[] ime = "Marko";
+			String[] prezime = "Markovic";
+		//zelimo ispisati ime i prezime 
+			System.out.println(ime + " " +  prezime);
+			*/
+		String[] grupa = {"Darko Gajic", "Miroslav Jagodic", "Dragana Pupac", "Tanja Stnkovic", "Mladen Tosic", "Blanaka Preradovic"	};	   
+		for(String clanovi : grupa) {
+		System.out.println(clanovi);
+	}
+	}
+}
