7ba14e9344931d58282b09388d4ef95f186392c1
Ranko <Ranko>
2016-12-13 12:07:15 +0100

    test

---
+++ Ranko07 - Korisnicki unos/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Kreiranje skener objekta
+				Scanner input = new Scanner(System.in);
+				
+				// Output prompt
+				System.out.println("Unesite vase ime: ");
+				
+				// Izvrsavanje skenera i arhiviranje rezultata
+				String line = input.nextLine();
+				// int value = input.nextInt();
+				// int value = input.nextDouble();
+				
+				
+	}
+
+}
